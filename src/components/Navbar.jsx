10b335efba5b22f1bf2eefede9963080ffd2e3eb
mscode04 <ms.shaheenkp@gmail.com.com>
2025-11-04 22:29:14 +0530

    updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -54,7 +54,7 @@ const Navbar = () => {
 
           {/* Desktop Navigation */}
           <div className="navbar-links">
-            <Link to="/home" className="navbar-link">
+            <Link to="/" className="navbar-link">
               Home
             </Link>
             <Link to="/about" className="navbar-link">
@@ -84,7 +84,7 @@ const Navbar = () => {
         {/* Mobile Navigation */}
         <div className={`navbar-mobile-menu ${mobileMenuOpen ? "open" : ""}`}>
           <Link
-            to="/home"
+            to="/"
             className="navbar-mobile-link"
             onClick={toggleMobileMenu}
           >
